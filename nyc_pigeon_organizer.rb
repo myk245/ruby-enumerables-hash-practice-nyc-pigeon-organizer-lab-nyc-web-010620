0c53e21e9c10f8e56392263ed80b9b17f009d04c
Cindy Kei <myk245@nyu.edu>
2019-12-01 22:24:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,12 @@
 def nyc_pigeon_organizer(data)
   pigeons = {}
-  data.map
+  data.each do |key, value|
+    value.each do |stats, array|
+      array.each do |name|
+        if pigeons.has_key?(key)
+          pigeons[name][key] << describe.to_s
+        else
+          pigeons[name][key] = [describe.to_s]
+
+
 end
